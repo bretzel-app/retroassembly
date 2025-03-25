52c295a94935bd305a7461552c1749014764a56e
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-03-25 14:10:03 +0800

    fix: fix roms in history page

--- src/pages/library/history/page.tsx
+++ src/pages/library/history/page.tsx
@@ -1,7 +1,10 @@
+import { HydrationBoundary } from 'jotai-ssr'
 import { getLaunchRecords } from '@/controllers/get-launch-records.ts'
+import { romsAtom } from '../atoms.ts'
 import { GameList } from '../components/game-list/game-list.tsx'
 import LibraryLayout from '../components/library-layout/library-layout.tsx'
 import { MainScrollArea } from '../components/main-scroll-area.tsx'
+import { getHydrateAtoms } from '../utils/hydrate-atoms.ts'
 
 export async function HistoryPage({ query }: { query: string }) {
   const page = Number.parseInt(new URLSearchParams(query).get('page') || '', 10) || 1
@@ -12,22 +15,27 @@ export async function HistoryPage({ query }: { query: string }) {
   }
 
   return (
-    <LibraryLayout title='History'>
-      <MainScrollArea className='z-1 relative flex flex-1' size='2'>
-        <main className='flex min-h-full w-full flex-col gap-5 p-4'>
-          <div className='relative flex justify-between px-4 pt-4'>
-            <h1 className='text-5xl font-[Oswald_Variable] font-semibold'>History</h1>
-            <div className='mt-4 flex items-center gap-2 text-zinc-400'>
-              <span className='icon-[mdi--bar-chart] text-black' />
-              Played
-              <span className='font-[DSEG7_Modern] font-bold text-rose-700'>{pagination.total}</span>
-              games.
+    <HydrationBoundary
+      hydrateAtoms={getHydrateAtoms({ override: [[romsAtom, roms]] })}
+      options={{ enableReHydrate: true }}
+    >
+      <LibraryLayout title='History'>
+        <MainScrollArea className='z-1 relative flex flex-1' size='2'>
+          <main className='flex min-h-full w-full flex-col gap-5 p-4'>
+            <div className='relative flex justify-between px-4 pt-4'>
+              <h1 className='text-5xl font-[Oswald_Variable] font-semibold'>History</h1>
+              <div className='mt-4 flex items-center gap-2 text-zinc-400'>
+                <span className='icon-[mdi--bar-chart] text-black' />
+                Played
+                <span className='font-[DSEG7_Modern] font-bold text-rose-700'>{pagination.total}</span>
+                games.
+              </div>
             </div>
-          </div>
-          <hr className='border-t-1 border-t-black/20' />
-          <GameList pagination={pagination} roms={roms} />
-        </main>
-      </MainScrollArea>
-    </LibraryLayout>
+            <hr className='border-t-1 border-t-black/20' />
+            <GameList pagination={pagination} />
+          </main>
+        </MainScrollArea>
+      </LibraryLayout>
+    </HydrationBoundary>
   )
 }
