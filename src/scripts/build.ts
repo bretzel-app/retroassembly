9a4a3518cd11888ca2b9c1f97a0a7360eaf4b17d
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-04-02 17:02:13 +0800

    fix: fix build

--- src/scripts/build.ts
+++ src/scripts/build.ts
@@ -1,6 +1,14 @@
-import { $ } from 'zx'
+import { template } from 'es-toolkit/compat'
+import { $, dotenv, fs } from 'zx'
 
 const $$ = $({ verbose: true })
+
+// prepare wrangler config
+const wranglerTemplate = await fs.readFile('wrangler.template.json', 'utf8')
+const compiled = template(wranglerTemplate)
+const env = { ...process.env, ...dotenv.loadSafe('.env') }
+const wranglerConfig = compiled(env)
+await fs.writeFile('wrangler.json', wranglerConfig, 'utf8')
 
 // build
 await $$`NODE_ENV=production VITE_EXPERIMENTAL_WAKU_ROUTER=true waku build --with-cloudflare`
--- src/scripts/dev/dev.ts
+++ src/scripts/dev/dev.ts
@@ -1,9 +1,17 @@
-import { $ } from 'zx'
+import { template } from 'es-toolkit/compat'
+import { $, dotenv, fs } from 'zx'
 
 const $$ = $({ verbose: true })
 
 while (true) {
   await $$`rm -rf node_modules/.vite`
+
+  // prepare wrangler config
+  const wranglerTemplate = await fs.readFile('wrangler.template.json', 'utf8')
+  const compiled = template(wranglerTemplate)
+  const env = { ...process.env, ...dotenv.loadSafe('.env') }
+  const wranglerConfig = compiled(env)
+  await fs.writeFile('wrangler.json', wranglerConfig, 'utf8')
 
   try {
     await $$`VITE_EXPERIMENTAL_WAKU_ROUTER=true waku dev --experimental-compress`
