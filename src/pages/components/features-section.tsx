7852b2700ca942458e10d7ded3279a9942f063fa
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-05-13 00:35:24 +0800

    feat: add feature card content

--- src/pages/components/features-section.tsx
+++ src/pages/components/features-section.tsx
@@ -1,35 +1,110 @@
 import clsx from 'clsx'
 import { range } from 'es-toolkit'
-import { Pong } from './pong.tsx'
+import { getPlatformDeviceBackground } from '@/utils/library.ts'
 
 export function FeaturesSection() {
   const features = [
     {
+      content: (
+        <div className='*:motion-preset-pulse-sm *:motion-duration-1000 flex size-full items-center justify-evenly bg-neutral-50'>
+          <span className='icon-[logos--chrome] size-14 ' />
+          <span className='icon-[logos--microsoft-edge] motion-delay-100 size-14' />
+          <span className='icon-[logos--safari] motion-delay-200 size-14' />
+          <span className='icon-[logos--firefox] motion-delay-300 size-14' />
+        </div>
+      ),
       description: 'Play retro games directly in your browser without additional software.',
       icon: 'icon-[mdi--web-box]',
       title: 'Browser-Based Gameplay',
     },
     {
+      content: (
+        <div className='relative flex size-full flex-col justify-center'>
+          <div className='grid size-full grid-cols-3 grid-rows-3 rounded'>
+            {['nes', 'snes', 'megadrive', 'gb', 'gba', 'gamegear', 'atari2600', 'arcade'].map((platform) => (
+              <div className='overflow-hidden' key={platform}>
+                <img
+                  alt={platform}
+                  className={clsx('size-full scale-110 object-contain object-center')}
+                  src={getPlatformDeviceBackground(platform)}
+                />
+              </div>
+            ))}
+            <div className='text-(--accent-9) flex items-center justify-center text-center font-semibold'>
+              ...and more!
+            </div>
+          </div>
+        </div>
+      ),
       description: 'Supports a wide range of retro gaming systems.',
       icon: 'icon-[mdi--space-invaders]',
       title: 'Multi-Platform Support',
     },
     {
+      content: (
+        <div className='flex size-full items-center justify-center gap-2'>
+          <span className='icon-[noto--laptop] size-16' />
+          <span className='icon-[svg-spinners--bars-scale-middle] w-12 opacity-50' />
+          <span className='icon-[noto--cloud] size-16' />
+          <span className='icon-[svg-spinners--bars-scale-middle] w-12 opacity-50' />
+          <span className='icon-[noto--desktop-computer] size-16' />
+        </div>
+      ),
       description: 'Sync your games and states then access them from anywhere.',
       icon: 'icon-[mdi--cloud]',
       title: 'Cloud Sync',
     },
     {
+      content: (
+        <div className='grid h-full grid-cols-3 place-items-center gap-2'>
+          <div className='aspect-square overflow-hidden rounded bg-neutral-200'>
+            <img
+              alt='mario'
+              className='size-full object-contain'
+              src='https://cdn.jsdelivr.net/gh/libretro-thumbnails/Nintendo_-_Nintendo_Entertainment_System@dbac0d8/Named_Boxarts/Super%20Mario%20Bros.%20(World).png'
+            />
+          </div>
+          <div className='aspect-square overflow-hidden rounded bg-neutral-200'>
+            <img
+              alt='sonic'
+              className='size-full object-contain'
+              src='https://cdn.jsdelivr.net/gh/libretro-thumbnails/Sega_-_Mega_Drive_-_Genesis@fa29730/Named_Boxarts/Sonic%20The%20Hedgehog%20(USA,%20Europe).png'
+            />
+          </div>
+          <div className='aspect-square overflow-hidden rounded bg-neutral-200'>
+            <img
+              alt='pitfall'
+              className='size-full object-contain'
+              src='https://cdn.jsdelivr.net/gh/libretro-thumbnails/Atari_-_2600@a6a54d3/Named_Boxarts/Pitfall%20II%20-%20Lost%20Caverns%20(USA).png'
+            />
+          </div>
+        </div>
+      ),
       description: "Automatically fetch game boxarts to enhance your library's visual appeal.",
       icon: 'icon-[mdi--drawing-box]',
       title: 'Automatic Boxart Retrieval',
     },
     {
+      content: (
+        <div className='grid size-full grid-cols-3 grid-rows-2 gap-2'>
+          <div className='rounded bg-neutral-100' />
+          <div className='rounded bg-neutral-100' />
+          <div className='rounded bg-neutral-100' />
+          <div className='rounded bg-neutral-100' />
+          <div className='rounded bg-neutral-100' />
+          <div className='rounded bg-neutral-100' />
+        </div>
+      ),
       description: 'Navigate seamlessly using a joystick without needing a mouse or keyboard.',
       icon: 'icon-[mdi--controller-round]',
       title: 'Joystick-Friendly Navigation',
     },
     {
+      content: (
+        <div className='flex size-full items-center justify-center'>
+          <span className='icon-[svg-spinners--clock] rotate-x-180 size-32 opacity-50' />
+        </div>
+      ),
       description: (
         <>
           Rewind gameplay using "R" on the keyboard or a controller button combination (<kbd>Select</kbd> +{' '}
@@ -43,10 +118,6 @@ export function FeaturesSection() {
 
   return (
     <section className='relative py-16'>
-      <div className='absolute inset-0 size-full'>
-        <Pong />
-      </div>
-
       <div className='mx-auto max-w-6xl '>
         <h2
           className='text-(--accent-9) mb-6 flex items-center justify-center gap-2 text-4xl font-[Roboto_Slab_Variable] font-semibold'
@@ -74,7 +145,7 @@ export function FeaturesSection() {
 
               <div className='flex items-center justify-center'>
                 <div className='rounded bg-white p-2 shadow-lg ring-1 ring-neutral-300'>
-                  <div className='w-100 h-60 bg-neutral-300' />
+                  <div className='w-100 h-60'>{feature.content}</div>
                 </div>
               </div>
             </div>
--- src/pages/components/hero-section/hero-section.tsx
+++ src/pages/components/hero-section/hero-section.tsx
@@ -1,6 +1,5 @@
 import { HeroImages } from './hero-images.tsx'
 import { HeroMain } from './hero-main.tsx'
-import { VisualEffect } from './visual-effect.tsx'
 
 export function HeroSection() {
   return (
@@ -8,7 +7,6 @@ export function HeroSection() {
       <div className='flex-1'>
         <div className='-z-1  absolute inset-0 size-full'>
           <div className='hero-bg absolute inset-0 size-full' />
-          <VisualEffect />
         </div>
 
         <div className='w-7xl mx-auto flex h-full gap-20 overflow-hidden'>
