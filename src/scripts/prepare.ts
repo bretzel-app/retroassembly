45d2b09f8388bf849e1571359e06f4a27bb930ee
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-07-10 11:04:14 +0800

    chore: debug env in build process

--- src/scripts/prepare.ts
+++ src/scripts/prepare.ts
@@ -2,8 +2,10 @@ import { $ } from 'execa'
 import fs from 'fs-extra'
 import { dataDirectory } from '../constants/env.ts'
 
-await $`simple-git-hooks`
+const $$ = $({ verbose: 'full' })
+await $$`simple-git-hooks`
 
 await fs.ensureDir(dataDirectory)
 
-await $`drizzle-kit --config=src/databases/library/drizzle.config.ts migrate`
+await $$`env`
+// await $`drizzle-kit --config=src/databases/library/drizzle.config.ts migrate`
