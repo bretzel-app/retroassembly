121c8cd9bd19eabb969bb3ed48c5a472d9891cb7
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-07-14 15:58:09 +0800

    fix: only enable pino transport on node

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { getRuntimeKey } from 'hono/adapter'
 import { contextStorage } from 'hono/context-storage'
 import { requestId } from 'hono/request-id'
 import { pinoLogger } from 'hono-pino'
@@ -8,7 +9,11 @@ import { globals } from '../middlewares/hono/globals.ts'
 import { vendors } from '../middlewares/hono/vendors.ts'
 
 const app = new Hono()
-app.use(requestId(), contextStorage(), pinoLogger({ pino: { transport: { target: 'hono-pino/debug-log' } } }))
+app.use(
+  requestId(),
+  contextStorage(),
+  pinoLogger(getRuntimeKey() === 'node' ? { pino: { transport: { target: 'hono-pino/debug-log' } } } : {}),
+)
 app.use(vendors(), globals(), auth())
 app.route('api', api)
 
