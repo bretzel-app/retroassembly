366b888eb4f2e42e23ae1b1c6155de6f5c439f09
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-07-19 23:59:17 +0800

    feat: add specific aspect ratios for skeletons by platforms

--- src/pages/library/components/game-entry/game-entry.tsx
+++ src/pages/library/components/game-entry/game-entry.tsx
@@ -9,6 +9,19 @@ import { GameEntryContextMenu } from './game-entry-context-menu.tsx'
 import { GamePlatform } from './game-platform.tsx'
 import { GameTitle } from './game-title.tsx'
 
+const skeletonClassnames = {
+  atari2600: '!aspect-[7/10] !h-full !w-auto',
+  famicom: '!aspect-[25/34] !h-full !w-auto',
+  genesis: '!aspect-[7/10] !h-full !w-auto',
+  megadrive: '!aspect-[7/10] !h-full !w-auto',
+  nes: '!aspect-[25/34] !h-full !w-auto',
+  sfc: '!aspect-[27/50] !h-full !w-auto',
+  sms: '!aspect-[12/17] !h-full !w-auto',
+  snes: '!aspect-[7/5] !h-auto !w-full',
+  wonderswan: '!aspect-[4/5] !h-full !w-auto',
+  wonderswancolor: '!aspect-[7/10] !h-full !w-auto',
+}
+
 export function GameEntry({ rom }) {
   const goodcodes = getRomGoodcodes(rom)
   const { data: cover, isLoading } = useRomCover(rom)
@@ -31,11 +44,11 @@ export function GameEntry({ rom }) {
             {isLoading ? (
               <motion.div
                 animate={{ opacity: 1 }}
-                className='absolute inset-0'
+                className='absolute inset-0 flex items-end justify-center'
                 exit={{ opacity: 0 }}
                 initial={{ opacity: 1 }}
               >
-                <Skeleton className='!size-full' loading />
+                <Skeleton className={skeletonClassnames[rom.platform] || '!size-full'} loading />
               </motion.div>
             ) : null}
           </AnimatePresence>
