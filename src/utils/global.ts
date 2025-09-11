01bf155195ac1f05b469235e8ef020d6785cab84
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-09-11 18:42:00 +0800

    chore: clean up packages

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,3 +1,4 @@
+import Clarity from '@microsoft/clarity'
 import { isBrowser } from 'es-toolkit'
 
 if (isBrowser()) {
@@ -9,23 +10,9 @@ if (isBrowser()) {
 
   const projectId = import.meta.env.RETROASSEMBLY_BUILD_TIME_VITE_CLARITY_PROJECT_ID
   if (projectId) {
-    /* eslint-disable max-params,sonarjs/no-nested-assignment,prefer-rest-params,unicorn/prefer-query-selector */
-    ;(function (c, l, a, r, i, _t, _y) {
-      c[a] =
-        c[a] ||
-        function () {
-          ;(c[a].q = c[a].q || []).push(arguments)
-        }
-      const t = l.createElement(r as 'script')
-      t.async = true
-      t.src = `https://www.clarity.ms/tag/${i}`
-      const y = l.getElementsByTagName(r)[0]
-      y.parentNode?.insertBefore(t, y)
-    })(globalThis, document, 'clarity', 'script', import.meta.env.RETROASSEMBLY_BUILD_TIME_VITE_CLARITY_PROJECT_ID)
-    /* eslint-enable */
-
+    Clarity.init(projectId)
     if (globalThis.CURRENT_USER?.id) {
-      globalThis.Clarity?.identify(globalThis.CURRENT_USER?.id, undefined, undefined, globalThis.CURRENT_USER?.email)
+      Clarity.identify(globalThis.CURRENT_USER?.id, undefined, undefined, globalThis.CURRENT_USER?.email)
     }
   }
 
