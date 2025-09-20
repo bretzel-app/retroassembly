8b1b9aa3ec9c1c2cd3803a8c142345869e651583
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-09-20 13:31:15 +0800

    fix: fix fetch error handling

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,18 @@
+import { attemptAsync } from 'es-toolkit'
 import { hc, parseResponse } from 'hono/client'
 import type { AppType } from './app'
 
 const baseUrl = '/'
 
-export const client = hc<AppType>(baseUrl).api.v1
+export const client = hc<AppType>(baseUrl, {
+  async fetch(input, init) {
+    const response = await fetch(input, init)
+    if (response.ok) {
+      return response
+    }
+    const [, json] = await attemptAsync(() => response.json())
+    throw json ?? response
+  },
+}).api.v1
 export type { InferRequestType } from 'hono'
 export { parseResponse }
