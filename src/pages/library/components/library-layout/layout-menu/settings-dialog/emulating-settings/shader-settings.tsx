1feabc4e4792bbd1700f0ba40c108def6af784ad
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-12-17 18:24:43 +0800

    feat: add more shaders

--- src/pages/library/components/library-layout/layout-menu/settings-dialog/emulating-settings/shader-settings.tsx
+++ src/pages/library/components/library-layout/layout-menu/settings-dialog/emulating-settings/shader-settings.tsx
@@ -1,22 +1,8 @@
 import { Card, HoverCard, RadioCards, Switch } from '@radix-ui/themes'
 import { useTranslation } from 'react-i18next'
 import { usePreference } from '#@/pages/library/hooks/use-preference.ts'
-import { getCDNUrl } from '#@/utils/isomorphic/cdn.ts'
 import { SettingsTitle } from '../settings-title.tsx'
-
-function getShaderThumbnail(shader: string) {
-  return getCDNUrl('libretro/docs', `/docs/image/shader/${shader}.png`)
-}
-
-const shaders = [
-  { id: '', name: 'disabled', thumbnail: '' },
-  { id: 'crt/crt-easymode', name: 'crt-easymode', thumbnail: getShaderThumbnail('crt/crt-easymode') },
-  { id: 'crt/crt-nes-mini', name: 'crt-nes-mini', thumbnail: getShaderThumbnail('crt/crt-nes-mini') },
-  { id: 'crt/crt-geom', name: 'crt-geom', thumbnail: getShaderThumbnail('crt/crt-geom') },
-  { id: 'handheld/dot', name: 'dot', thumbnail: getShaderThumbnail('handheld/dot') },
-  { id: 'handheld/lcd3x', name: 'lcd3x', thumbnail: getShaderThumbnail('handheld/lcd-3x') },
-  { id: 'xbrz/xbrz-freescale', name: 'xbrz-freescale', thumbnail: getShaderThumbnail('xbrz/4xbrz') },
-]
+import { shaders } from './shaders.ts'
 
 export function ShaderSettings() {
   const { t } = useTranslation()
